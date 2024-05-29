1429cc7c7e8383d46e4be1d036e512571a783fe9
Jozef Duda <j.duda56@outlook.com>
2024-05-29 08:47:41 +0100

    updated ecg layout to include waveform, button and title

--- src/mvp/views/ecg_view/ecg_view.cpp
+++ src/mvp/views/ecg_view/ecg_view.cpp
@@ -5,6 +5,12 @@
 #include "interfaces/base_view/i_base_view.h"
 #include <functional>
 
+// Use 10px as safe are.
+#define SIZE_SAFE_PX                   20U
+
+// Use 100px for header.
+#define TOP_ROW_HEIGHT_PX              100U
+
 #define BACKGROUN_COLOR                0x001d3d
 #define CHART_BACKGROUND_COLOR         0x003566
 
@@ -12,21 +18,22 @@
 
 namespace LvUi {
 
-
-    EcgView::EcgView(int32_t t_view_w, int32_t t_view_h)
+    EcgView::EcgView(const int32_t t_view_w, const int32_t t_view_h)
     {
-        int32_t width_length = (t_view_w - 10) / 4;   // Use 10px as safe are.
-        int32_t centre_hight = t_view_h - 110;        // 100px for header and 10 for safe area.
+        // Container will include 4 columns + 1 column for any unused space.
+        // And, 2 rows + 1 row for any unused space.
+        // int32_t width_length = (t_view_w - SIZE_SAFE_PX) / 4;
+        // int32_t centre_hight = t_view_h - (TOP_ROW_HEIGHT_PX);
 
-        // configure container.
-        int32_t col_dsc[] = {width_length, width_length, width_length, width_length, LV_GRID_TEMPLATE_LAST};
-        int32_t row_dsc[] = {100, centre_hight, LV_GRID_TEMPLATE_LAST};
+        // // configure container.
+        // int32_t col_dsc[] = {60, LV_GRID_FR(1), LV_GRID_FR(2), LV_GRID_TEMPLATE_LAST};
+        // int32_t row_dsc[] = {50, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
 
-        auto col_size = sizeof(col_dsc) / sizeof(col_dsc[0]);
-        auto row_size = sizeof(row_dsc) / sizeof(row_dsc[0]);
+        // auto col_size = sizeof(col_dsc) / sizeof(col_dsc[0]);
+        // auto row_size = sizeof(row_dsc) / sizeof(row_dsc[0]);
 
-        std::copy(col_dsc, col_dsc + col_size, std::back_inserter(m_container_col_dsc));
-        std::copy(col_dsc, col_dsc + col_size, std::back_inserter(m_container_row_dsc));
+        // std::copy(col_dsc, col_dsc + col_size, std::back_inserter(m_container_col_dsc));
+        // std::copy(col_dsc, col_dsc + col_size, std::back_inserter(m_container_row_dsc));
 
         create(t_view_w, t_view_h);
     }
@@ -42,62 +49,65 @@ namespace LvUi {
     }
     */
 
-
-    void EcgView::create(int32_t t_view_w, int32_t t_view_h)
+    void LvUi::EcgView::initEcgChart(const int32_t t_view_w, const int32_t t_view_h)
     {
-        static lv_obj_t * chart;
-        static lv_chart_series_t * ser;
-        static lv_chart_cursor_t * cursor;
+        m_ecg_chart = lv_chart_create(m_container);
+        lv_obj_set_grid_cell(m_ecg_chart, LV_GRID_ALIGN_STRETCH, 0, 3, LV_GRID_ALIGN_STRETCH, 1, 1);
+        lv_obj_set_style_radius(m_ecg_chart, 10, LV_PART_MAIN);
+        lv_obj_set_style_border_color(m_ecg_chart, lv_color_hex(CHART_BACKGROUND_COLOR), LV_PART_MAIN);
+        lv_obj_set_style_bg_color(m_ecg_chart, lv_color_hex(CHART_BACKGROUND_COLOR), LV_PART_MAIN);
 
+
+        m_ecg_series = lv_chart_add_series(m_ecg_chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
+
+        // Add demo data.
+        for(uint32_t i = 0; i < 200; i++) {
+            lv_chart_set_next_value(m_ecg_chart, m_ecg_series, lv_rand(-20, 70));
+        }
+    }
+
+
+    void EcgView::create(const int32_t t_view_w, const int32_t t_view_h)
+    {
         // Main view background color.
         lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(BACKGROUN_COLOR), LV_PART_MAIN);
 
+        auto container_width = t_view_w - SIZE_SAFE_PX;
+        auto container_height = t_view_h - SIZE_SAFE_PX;
+
+        // configure container.
+        static int32_t col_dsc[] = {420, 420, 420, LV_GRID_TEMPLATE_LAST};
+        static int32_t row_dsc[] = {50, 600, LV_GRID_TEMPLATE_LAST};
+
         // Setup main container.
         m_container = lv_obj_create(lv_screen_active());
-        lv_obj_set_style_grid_column_dsc_array(m_container, &m_container_col_dsc[0], 0);
-        lv_obj_set_style_grid_row_dsc_array(m_container, &m_container_row_dsc[0], 0);
-        lv_obj_set_size(m_container, t_view_w, t_view_h);
+        lv_obj_set_style_grid_column_dsc_array(m_container, col_dsc, 0);
+        lv_obj_set_style_grid_row_dsc_array(m_container, row_dsc, 0);
+        lv_obj_set_size(m_container, container_width, container_height);
         lv_obj_center(m_container);
         lv_obj_set_layout(m_container, LV_LAYOUT_GRID);
         lv_obj_set_style_bg_color(m_container, lv_color_hex(BACKGROUN_COLOR), LV_PART_MAIN);
         lv_obj_set_style_border_color(m_container, lv_color_hex(BACKGROUN_COLOR), LV_PART_MAIN);
+        lv_obj_align(m_container, LV_ALIGN_CENTER, 0, 0);
 
 
-
-
-        // Initialize chart.
-        chart = lv_chart_create(m_container);
-        lv_obj_set_size(chart, 900, 350);
-        lv_obj_set_grid_cell(chart, LV_GRID_ALIGN_STRETCH, 0, 4, LV_GRID_ALIGN_STRETCH, 1, 1);
-
-        static lv_style_t chart_style;
-        lv_style_init(&chart_style);
-        lv_style_set_radius(&chart_style, 10);
-        lv_style_set_border_color(&chart_style,lv_color_hex(CHART_BACKGROUND_COLOR));
-        lv_style_set_bg_color(&chart_style, lv_color_hex(CHART_BACKGROUND_COLOR));
-        lv_obj_add_style(chart, &chart_style, LV_PART_MAIN);
-
-        ser = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
-        uint32_t i;
-        for(i = 0; i < 200; i++) {
-            lv_chart_set_next_value(chart, ser, lv_rand(-20, 70));
-        }
-
-        // ESG label and background object.
-        static lv_style_t lb_bg_style;
-        lv_style_init(&lb_bg_style);
-        lv_style_set_size(&lb_bg_style, 200, 25);
-        lv_style_set_border_color(&lb_bg_style,lv_color_hex(CHART_BACKGROUND_COLOR));
-        lv_style_set_bg_color(&lb_bg_style, lv_color_hex(CHART_BACKGROUND_COLOR));
-
-        lv_obj_t * lb_bg_obj = lv_obj_create(chart);
-        lv_obj_add_style(lb_bg_obj, &lb_bg_style, 0);
-
-        lv_obj_t * chart_lb = lv_label_create(lb_bg_obj);
+        // Title.
+        lv_obj_t *chart_lb = lv_label_create(m_container);
         lv_label_set_text(chart_lb, "ECG Waveform");
-        lv_obj_set_style_text_align(chart_lb, LV_TEXT_ALIGN_LEFT, 0);
-        lv_obj_align(chart_lb, LV_ALIGN_CENTER, 0, 0);
         lv_obj_set_style_text_color(chart_lb, lv_color_hex(0xffffff), LV_PART_MAIN);
+        lv_obj_set_style_text_font(chart_lb, &lv_font_montserrat_40, LV_PART_MAIN);
+        lv_obj_set_grid_cell(chart_lb, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_START, 0, 1);
+
+        // Control button.
+        control_btn = lv_button_create(m_container);
+        lv_obj_set_grid_cell(control_btn, LV_GRID_ALIGN_CENTER, 0, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+
+        static lv_obj_t *control_btn_label = lv_label_create(control_btn);
+        lv_label_set_text(control_btn_label, "Start / Stop");
+
+        // Initialize ecg chart.
+        initEcgChart(container_width, container_height);
+
 
 
 
@@ -134,6 +144,7 @@ namespace LvUi {
 
 
     }
+
 
 
 
--- src/mvp/views/ecg_view/ecg_view.h
+++ src/mvp/views/ecg_view/ecg_view.h
@@ -9,20 +9,21 @@ namespace LvUi {
 
     class EcgView : public IBaseView {
         private:
-            std::vector<int32_t> m_container_col_dsc;      // container columns.
-            std::vector<int32_t> m_container_row_dsc;      // Container rows.
-            lv_obj_t * m_container;                        // Container.
-
-            // lv_obj_t* mainBtn;
-            // lv_obj_t* mainLabel;
+            // std::vector<int32_t> m_container_col_dsc;       // Container columns.
+            // std::vector<int32_t> m_container_row_dsc;       // Container rows.
+            lv_obj_t *m_container;                          // Container.
+            lv_obj_t *m_ecg_chart;                          // ECG chart.
+            lv_chart_series_t *m_ecg_series;                // ECG data series.
+            lv_obj_t *control_btn;                          // Enable / disable button.
 
         private:
+            void initEcgChart(const int32_t t_view_w, const int32_t t_view_h);
 
         public:
-            EcgView(int32_t t_view_w, int32_t t_view_h);
+            EcgView(const int32_t t_view_w, const int32_t t_view_h);
             ~EcgView() = default;
 
-            void create(int32_t t_view_w, int32_t t_view_h);
+            void create(const int32_t t_view_w, const int32_t t_view_h);
 
             void updateLabel1(std::string str);
     };
